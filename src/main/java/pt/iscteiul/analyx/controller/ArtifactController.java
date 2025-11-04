82b750bd503bcc2865b9aa87ad91e85450c2cd45
Sergio Eduardo D Oliveira <sergiowww@gmail.com>
2025-11-04 21:58:19 +0000

    #34 configuração do spring mvc

--- src/main/java/pt/iscteiul/analyx/controller/ArtifactController.java
+++ src/main/java/pt/iscteiul/analyx/controller/ArtifactController.java
@@ -1,62 +1,15 @@
 package pt.iscteiul.analyx.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import pt.iscteiul.analyx.entity.Artifact;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/artifact")
+@Controller
 public class ArtifactController {
 
-	@GetMapping
-	public List<Artifact> listAll() {
-		return null;
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("hello", "teste");
+		return "index";
 	}
-
-	@GetMapping("/{id}")
-	public Artifact getArtifact(Integer id) {
-		return null;
-	}
-
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> removeArtifact(Integer id) {
-		return null;
-	}
-
-	/**
-	 * Atualização total dos dados
-	 * @param id
-	 * @param artifact
-	 * @return
-	 */
-	@PutMapping("/{id}")
-	public Artifact update(Integer id, Artifact artifact) {
-		return null;
-	}
-
-	/**
-	 * Atualização parcial de dados
-	 * @param id
-	 * @param artifact
-	 * @return
-	 */
-	@PatchMapping("/{id}")
-	public Artifact updatePatch(Integer id, Artifact artifact) {
-		return null;
-	}
-
-	@PostMapping
-	public Artifact createArtifact(Artifact artifact) {
-		return null;
-	}
-
-
 }
